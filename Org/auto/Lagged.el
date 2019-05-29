732eb0b21fd3314c377f1a2e8fc27fb8b3fb349c
Georgi Boshnakov <georgi.boshnakov@manchester.ac.uk>
2019-05-29 11:00:12 +0100

    further testing of methods for Lagged2d

--- Org/auto/Lagged.el
+++ Org/auto/Lagged.el
@@ -5,12 +5,12 @@
                      '(("article" "11pt" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "left=1.8cm" "right=1.8cm" "top=2cm" "bottom=2cm") ("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,25 +31,25 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgc371a54"
-    "sec:orge6d2c91"
-    "sec:org4ec62a3"
-    "sec:org06e120a"
-    "sec:org24a9d23"
-    "sec:org0a34409"
-    "sec:org49c6a69"
-    "sec:org2bdf302"
-    "sec:org5b96f3e"
-    "sec:org6d0781c"
-    "sec:org0d28112"
-    "sec:org5b4aa19"
-    "sec:orgc12a672"
-    "sec:org68a3c70"
-    "sec:org472d3cf"
-    "sec:org5e1ae2d"
-    "sec:org27b9a04"
-    "sec:org1ca5f3c"
-    "sec:org0ce69ec"
-    "sec:org8e6c5d3"))
+    "sec:orgfa847ad"
+    "sec:org5157877"
+    "sec:org677e7b4"
+    "sec:orgaba311f"
+    "sec:org45bda62"
+    "sec:org86c31c2"
+    "sec:org83c8cc5"
+    "sec:org4838c8a"
+    "sec:org333a640"
+    "sec:orgeb8e692"
+    "sec:orgaa40c0d"
+    "sec:org1436e9d"
+    "sec:org4f09cdf"
+    "sec:orgef70436"
+    "sec:orgfad224c"
+    "sec:org79ef637"
+    "sec:org6334117"
+    "sec:org26c9eed"
+    "sec:org0ce2bb8"
+    "sec:org269ad87"))
  :latex)
 
